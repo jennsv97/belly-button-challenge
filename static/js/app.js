1ad162bee57c69f5a740ded12d1e5e825b830e4d
jennsv97 <vegajennifer91@gmail.com>
2025-03-11 13:10:37 -0700

    dropdown code and bubble chart adjustments

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ function buildMetadata(sample) {
 
 
     // Filter the metadata for the object with the desired sample number
-    let result = metadata.find(obj => obj.id == sample);
+    let result = metadata.filter(obj => obj.id == sample)[0];
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
@@ -37,7 +37,7 @@ function buildCharts(sample) {
 
 
     // Filter the samples for the object with the desired sample number
-    let results = samples.find(obj => obj.id == sample);
+    let results = samples.filter(obj => obj.id == sample)[0];
 
 
     // Get the otu_ids, otu_labels, and sample_values
@@ -55,15 +55,14 @@ function buildCharts(sample) {
       markers: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "Rainbow",
-        opacity: 0.7 
+        colorscale: "Earth"
       }
     }
     //Layout ( copied the names on the provided UCI Dashboard images )
     let bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       xaxis: {title: "OTU ID"},
-      yaxis: {title: "Number of Bacteria"},
+      yaxis: {title: "Number of Bacteria", range: [0, 250]},
       hovermode: "closest"
     };
 
@@ -90,7 +89,6 @@ function buildCharts(sample) {
     let barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       margin: { t: 30, l: 150 },
-    
     };
 
     // Render the Bar Chart
@@ -104,20 +102,24 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    sampleNames.forEach((sample) => {
+      dropdown.append("option").text(`Sample ${sample}`).property("value", sample);
+    });
 
     // Get the first sample from the list
-
+    let firstSample = sampleNames[0];
 
     // Build charts and metadata panel with the first sample
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
 
   });
 }
@@ -125,7 +127,8 @@ function init() {
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
