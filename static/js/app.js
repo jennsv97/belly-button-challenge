6878ac0634886d1c1f5f95151a79e07fa4069161
jennsv97 <vegajennifer91@gmail.com>
2025-03-11 11:45:33 -0700

    build Metadata code complete

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,27 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
+    let metadata = data.metadata;
 
 
     // Filter the metadata for the object with the desired sample number
+    let result = metadata.find(obj => obj.id == sample);
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    // I used chatGPT to create my panel.append loop
+    if (result) {
+      Object.entries(result).forEach(([key, value]) => {
+        panel.append("p").text(`${key}: ${value}`);
+      });
+    }
 
   });
 }
@@ -25,9 +33,11 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
+    let samples = data.samples;
 
 
     // Filter the samples for the object with the desired sample number
+    let result = samples.find(obj => obj.id == sample);
 
 
     // Get the otu_ids, otu_labels, and sample_values
